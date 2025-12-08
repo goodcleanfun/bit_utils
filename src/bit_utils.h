e743320b56ebf92078658328e43545f44c8d30cb
Al <albarrentine@gmail.com>
2025-12-08 01:16:57 -0500

    [int] using standard int sizes

--- src/bit_utils.h
+++ src/bit_utils.h
@@ -1,6 +1,9 @@
 #ifndef BIT_UTILS_H
 #define BIT_UTILS_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 // MSVC implementation by Pavel P (https://gist.github.com/pps83/3210a2f980fd02bb2ba2e5a1fc4a2ef0)
 #if defined(_MSC_VER) && !defined(__clang__)
 #include <intrin.h>
@@ -116,39 +119,31 @@ static inline uint32_t u32_ceil_div(uint32_t x, uint32_t y) {
 }
 
 #define floor_log2(x) _Generic((x) \
-    , unsigned: u32_floor_log2 \
-    , int: u32_floor_log2 \
-    , unsigned long: u32_floor_log2 \
-    , long: u32_floor_log2 \
-    , unsigned long long: u64_floor_log2 \
-    , long long: u64_floor_log2 \
+    , uint32_t: u32_floor_log2 \
+    , int32_t: u32_floor_log2 \
+    , uint64_t: u64_floor_log2 \
+    , int64_t: u64_floor_log2 \
     )(x)
 
 #define ceil_log2(x) _Generic((x) \
-    , unsigned: u32_ceil_log2 \
-    , int: u32_ceil_log2 \
-    , unsigned long: u32_ceil_log2 \
-    , long: u32_ceil_log2 \
-    , unsigned long long: u64_ceil_log2 \
-    , long long: u64_ceil_log2 \
+    , uint32_t: u32_ceil_log2 \
+    , int32_t: u32_ceil_log2 \
+    , uint64_t: u64_ceil_log2 \
+    , int64_t: u64_ceil_log2 \
     )(x)
 
 #define hyper_ceil(x) _Generic((x) \
-    , unsigned: u32_hyper_ceil \
-    , int: u32_hyper_ceil \
-    , unsigned long: u32_hyper_ceil \
-    , long: u32_hyper_ceil \
-    , unsigned long long: u64_hyper_ceil \
-    , long long: u64_hyper_ceil \
+    , uint32_t: u32_hyper_ceil \
+    , int32_t: u32_hyper_ceil \
+    , uint64_t: u64_hyper_ceil \
+    , int64_t: u64_hyper_ceil \
     )(x)
 
 #define ceil_div(x, y) _Generic((x, y) \
-    , unsigned: u32_ceil_div \
-    , int: u32_ceil_div \
-    , unsigned long: u32_ceil_div \
-    , long: u32_ceil_div \
-    , unsigned long long: u64_ceil_div \
-    , long long: u64_ceil_div \
+    , uint32_t: u32_ceil_div \
+    , int32_t: u32_ceil_div \
+    , uint64_t: u64_ceil_div \
+    , int64_t: u64_ceil_div \
     )(x, y)
 
 
@@ -185,17 +180,14 @@ static inline bool u64_is_power_of_two(uint64_t x) {
 }
 
 #define is_power_of_two(x) _Generic((x) \
-    , unsigned: u32_is_power_of_two \
-    , int: i32_is_power_of_two \
-    , unsigned long: u32_is_power_of_two \
-    , long: i32_is_power_of_two \
-    , unsigned long long: u64_is_power_of_two \
-    , long long: i64_is_power_of_two \
-    , char: i8_is_power_of_two \
-    , unsigned char: u8_is_power_of_two \
-    , short: i16_is_power_of_two \
-    , unsigned short: u16_is_power_of_two \
+    , uint32_t: u32_is_power_of_two \
+    , int32_t: i32_is_power_of_two \
+    , uint64_t: u64_is_power_of_two \
+    , int64_t: i64_is_power_of_two \
+    , int8_t: i8_is_power_of_two \
+    , uint8_t: u8_is_power_of_two \
+    , int16_t: i16_is_power_of_two \
+    , uint16_t: u16_is_power_of_two \
     )(x)
-
 
 #endif // BIT_UTILS_H
